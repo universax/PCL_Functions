167f79b7c9224a5d9ab3f455dda24530d31431b9
Yohei Kajiwara <universax80@gmail.com>
2020-04-17 14:06:21 +0900

    update

---
+++ Typedef.h
@@ -0,0 +1,8 @@
+#pragma once
+using namespace std;
+
+typedef pcl::PointXYZRGB PointType;
+typedef pcl::PointXYZI PointTypeI;
+typedef pcl::PointNormal PointNormalType;
+
+#define NUM_INPUT_SOURCE 1
